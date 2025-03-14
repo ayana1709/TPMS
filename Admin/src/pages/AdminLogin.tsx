f04e799dfaa038e6c2b2cb334ac59f9cd21db692
kaleab gemechu <kgemechu908@gmail.com>
2025-03-14 19:21:28 +0300

    updated the login page

--- Admin/src/pages/AdminLogin.tsx
+++ Admin/src/pages/AdminLogin.tsx
@@ -10,10 +10,10 @@ const LoginPage: React.FC = () => {
       <div className="flex bg-white w-[98%] h-[95%] m-auto border border-green-500 rounded-md overflow-hidden">
         {/* Left Side */}
         <div className="w-1/2 bg-login bg-center bg-cover text-white flex flex-col justify-center items-center p-10 rounded-md">
-          <h1 className="text-4xl font-bold">
+          <h1 className="text-4xl text-center font-bold font-poppins">
             Traffic Police Management System
           </h1>
-          <p className="mt-4 text-center text-lg">
+          <p className="mt-4 text-left text-lg font-poppins">
             The Traffic Penalty Management System simplifies tracking, managing,
             and processing traffic fines, ensuring efficiency and transparency
             for both authorities and the public.
@@ -22,21 +22,21 @@ const LoginPage: React.FC = () => {
         {/* Right Side */}
         <div className="w-1/2 h-screen flex flex-col justify-center items-center">
           <div className="w-[65%] p-4 h-[70%] flex flex-col justify-around border border-green-500 rounded-md hover:shadow-sm shadow-green-200 transition-all duration-500">
-            <h2 className="text-3xl font-semibold mb-6 text-green-600">
+            <h2 className="text-3xl font-semibold font-poppins mb-6 text-green-600">
               Sign In
             </h2>
             <div className="relative">
               <input
                 type="text"
                 placeholder="Username"
-                className="w-full outline-none ring-0 border-2 border-green-600 focus:border-green-700 p-3 rounded-lg mb-4 transition-all duration-200"
+                className="placeholder:text-lg w-full outline-none ring-0 border-2 border-green-600 focus:border-green-700 p-3 rounded-lg mb-4 transition-all duration-200"
               />
             </div>
             <div className="relative">
               <input
                 type={`${showPassword ? "password" : "text"}`}
                 placeholder="Password"
-                className="w-full outline-none ring-0 border-2 border-green-600 focus:border-green-700 p-3 rounded-lg mb-4 transition-all duration-200"
+                className="w-full placeholder:text-lg outline-none ring-0 border-2 border-green-600 focus:border-green-700 p-3 rounded-lg mb-4 transition-all duration-200"
               />
               <div
                 onClick={() => setShowPassword(!showPassword)}
@@ -50,10 +50,10 @@ const LoginPage: React.FC = () => {
               </div>
             </div>
             <div>
-              <button className="w-full bg-green-600 text-white p-3 rounded-lg hover:bg-green-700">
+              <button className="w-full font-poppins bg-green-600 text-white p-3 rounded-lg hover:bg-green-700">
                 Login
               </button>
-              <p className="mt-4 text-gray-500 cursor-pointer">
+              <p className="mt-4 text-gray-500 cursor-pointer font-poppins">
                 Forgot your password?
               </p>
             </div>
