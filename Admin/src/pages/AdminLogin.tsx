3059373cf4cfb13bcea83191296199f52a1b5982
kaleab gemechu <kgemechu908@gmail.com>
2025-03-14 18:57:41 +0300

    updated the login page

--- Admin/src/pages/AdminLogin.tsx
+++ Admin/src/pages/AdminLogin.tsx
@@ -1,6 +1,10 @@
 // import { useState } from "react";
+import { useState } from "react";
+import { HiMiniEyeSlash } from "react-icons/hi2";
+import { HiOutlineEye } from "react-icons/hi2";
 
 const LoginPage: React.FC = () => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="flex h-screen overflow-hidden">
       <div className="flex bg-white w-[98%] h-[95%] m-auto border border-green-500 rounded-md overflow-hidden">
@@ -21,19 +25,29 @@ const LoginPage: React.FC = () => {
             <h2 className="text-3xl font-semibold mb-6 text-green-600">
               Sign In
             </h2>
-            <div>
+            <div className="relative">
               <input
                 type="text"
                 placeholder="Username"
                 className="w-full outline-none ring-0 border-2 border-green-600 focus:border-green-700 p-3 rounded-lg mb-4 transition-all duration-200"
               />
             </div>
-            <div>
+            <div className="relative">
               <input
-                type="password"
+                type={`${showPassword ? "password" : "text"}`}
                 placeholder="Password"
                 className="w-full outline-none ring-0 border-2 border-green-600 focus:border-green-700 p-3 rounded-lg mb-4 transition-all duration-200"
               />
+              <div
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute top-[40%] right-4 -translate-y-1/2 cursor-pointer"
+              >
+                {showPassword ? (
+                  <HiOutlineEye size={20} className="text-green-800" />
+                ) : (
+                  <HiMiniEyeSlash size={20} className="text-green-800" />
+                )}
+              </div>
             </div>
             <div>
               <button className="w-full bg-green-600 text-white p-3 rounded-lg hover:bg-green-700">
