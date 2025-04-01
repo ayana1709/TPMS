29b678b2a3077d71c87b39fc3de34b42f5d15311
kaleab gemechu <kgemechu908@gmail.com>
2025-04-01 16:52:58 +0300

    added the logout functionality

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Auth\LoginRegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,3 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/login', [LoginRegisterController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+
+    return response()->json([
+        'message' => 'Logged out successfully'
+    ], 200);
+});
+
