9c63021884d4d053af8cd97536a52298e988d8ff
kaleab gemechu <kgemechu908@gmail.com>
2025-04-02 02:21:19 +0300

    updated the login page

--- frontend/admin/src/components/Login.jsx
+++ frontend/admin/src/components/Login.jsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useStores } from "@/contexts/storeContext";
+import { motion } from "framer-motion";
 
 export default function Login() {
   const [username, setUsername] = useState("");
@@ -32,48 +33,64 @@ export default function Login() {
   }
 
   return (
-    <div className="relative flex justify-center items-center h-screen bg-[#082f49]">
-      <div className="absolute right-20 h-[90%] w-[40%] bg-sky-900 p-8 rounded-xl shadow-2xl overflow-hidden">
-        <h2 className="text-4xl font-bold text-center mb-2 text-white tracking-widest uppercase">
-          Login
-        </h2>
-        {error && <p className="text-red-500 text-center mb-4">{error}</p>}
-        <form onSubmit={handleLogin} className="relative h-full">
-          <div className="flex flex-col gap-4 absolute w-[80%] top-[30%] left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="text" className="text-xl text-white">
-                User Name
-              </Label>
-              <Input
-                type="text"
-                className="w-full py-6 border-2 border-gray-200 focus:border-green-500 rounded-md focus:outline-none focus:ring-0 transition-all duration-150"
-                placeholder="User Name"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
+    <div className="relative w-full flex justify-center items-center h-screen bg-[#082f49] overflow-hidden">
+      <div className="bg-[url('/images/addis.png')] bg-cover bg-center w-[95%] h-[95%] rounded-lg p-4">
+        <motion.img
+          src="/images/kido.png" // Replace with your actual image URL
+          alt="Moving Image"
+          className="absolute -bottom-6 w-60 h-auto"
+          animate={{ x: ["-100vw", "100vw"] }} // Moves from left to right infinitely
+          transition={{ duration: 15, repeat: Infinity, ease: "easeInOut" }} // Loops infinitely
+        />
+        <div class="absolute z-[9] inset-0 bg-gradient-to-b from-black/50 to-black/50"></div>
+
+        <motion.div
+          initial={{ x: -300, opacity: 0 }} // Start 200px left and invisible
+          animate={{ x: 0, opacity: 1 }} // Move to the original position and become visible
+          transition={{ duration: 0.8, ease: "easeOut" }} // Smooth animation
+          className="absolute z-[999] left-1/2 -translate-x-1/2 h-[90%] w-[35%] bg-[rgba(0,0,0,0.5)] p-8 rounded-xl shadow-2xl overflow-hidden"
+        >
+          <h2 className="text-4xl font-bold text-center mb-2 text-white tracking-widest uppercase">
+            Login
+          </h2>
+          {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+          <form onSubmit={handleLogin} className="relative h-full">
+            <div className="flex flex-col gap-6 absolute w-[80%] top-[30%] left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="text" className="text-lg text-gray-300">
+                  User Name
+                </Label>
+                <Input
+                  type="text"
+                  className="placeholder:text-gray-200 text-gray-200 w-full py-6 border-2 border-gray-200 focus:border-green-500 rounded-md focus:outline-none focus:ring-0 transition-all duration-150"
+                  placeholder="User Name"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="password" className="text-lg text-gray-300">
+                  Password
+                </Label>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  className="placeholder:text-gray-200 text-gray-200 inline-block w-full border-2 py-6 border-gray-200 focus:border-green-500 rounded-md focus:outline-none focus:ring-0 transition-all duration-150"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <Button
+                type="submit"
+                className="w-full py-6 bg-gray-900 text-white tracking-[4px] uppercase hover:bg-gray-950 cursor-pointer rounded-lg transition-all duration-500"
+              >
+                Login
+              </Button>
             </div>
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="password" className="text-xl text-white">
-                Password
-              </Label>
-              <Input
-                type="password"
-                placeholder="Password"
-                className="inline-block w-full border-2 py-6 border-gray-200 focus:border-green-500 rounded-md focus:outline-none focus:ring-0 transition-all duration-150"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <Button
-              type="submit"
-              className="w-full py-6 bg-sky-800 text-white tracking-wide uppercase hover:bg-sky-950 cursor-pointer rounded-lg transition-all duration-200"
-            >
-              Login
-            </Button>
-          </div>
-        </form>
+          </form>
+        </motion.div>
       </div>
     </div>
   );
