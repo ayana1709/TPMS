ce1cf1945a13b7e17e92399e9d446686bd1fac13
kaleab gemechu <kgemechu908@gmail.com>
2025-04-07 12:04:56 +0300

    setup the table component for managers

--- frontend/admin/src/pages/ViewManagers.jsx
+++ frontend/admin/src/pages/ViewManagers.jsx
@@ -1,5 +1,16 @@
+import { ManagerTable } from "@/components/ManagerTable";
+
 function ViewManagers() {
-  return <div>kaleab</div>;
+  return (
+    <div>
+      <div className="max-w-[95%] mx-auto">
+        <h2 className="text-gray-800 py-6 dark:text-gray-200 text-2xl uppercase font-semibold tracking-[14px] text-center">
+          managers
+        </h2>
+        <ManagerTable />
+      </div>
+    </div>
+  );
 }
 
 export default ViewManagers;
