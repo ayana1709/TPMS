b8d32cdefdb39d3a120a6393fae2c9f643c34b27
firew <firewfilpos27@gmail.com>
2025-04-22 02:46:27 -0400

    model violtion

--- backend/app/Models/Violation.php
+++ backend/app/Models/Violation.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Violation extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'driver_id', 'car_id', 'rule_id', 'officer_id', 'penalty_amount',
+        'signed', 'signed_at', 'paid', 'paid_at',
+    ];
+
+    public function driver() { 
+        return $this->belongsTo(User::class, 'driver_id');
+               
+    }
+    public function officer() { 
+
+        return $this->belongsTo(User::class, 'officer_id');
+     }
+    public function car()     { 
+        return $this->belongsTo(Car::class); 
+    
+    }
+    public function rule()    {
+        
+         return $this->belongsTo(TrafficRule::class); 
+        }
 }
+
+
+
+
