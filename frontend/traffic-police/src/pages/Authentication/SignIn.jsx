f30db5dbe03d9ecaacc82940334b2be63a4bd398
ayana1709 <ayanabashachali@gmail.com>
2025-05-07 20:56:11 +0300

    signin  page

--- frontend/traffic-police/src/pages/Authentication/SignIn.jsx
+++ frontend/traffic-police/src/pages/Authentication/SignIn.jsx
@@ -6,6 +6,7 @@ import { Button } from '../../components/ui/button';
 import { Input } from '../../components/ui/input';
 import { Label } from '../../components/ui/label';
 import { Switch } from '../../components/ui/switch';
+import api from '../../api';
 
 const SignIn = () => {
   const navigate = useNavigate();
